abe15e54071c46a4823efd9be50bb765795bb8d6
Ashutosh Kumar <ashutoshbiotech2020@gmail.com>
2024-08-24 14:44:26 +0530

    yt clone

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-const nav_btns = [
-	"All", "Music", "Sports", "Films", "Courses", "Comedy", "News", "Live", "Gaming", "Podcast", "Trailers", "Vlogs", "Learning", "Fashion", "Animals", "Anime", "Automotive", "Travel", "Education", "Entertainment", "Science", "Technology", "Food", "History", "Comedy", "Movies", "News", "Sports", "Gaming", "Fashion", "Animals", "Anime", "Automotive", "Travel", "Education", "Entertainment", "Science"
-]
-
-const nav = document.querySelector("#nav-btns");
-nav_btns.forEach(btn => {
-	nav.innerHTML += `<div class="nav-btn">${btn}</div>`;
-	console.log("hello wor;d");
-})
